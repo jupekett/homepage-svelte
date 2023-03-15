c63c880c2bc4e8583033775f1afcce07a710c583
jupekett <jupekett@student.jyu.fi>
2023-03-15 22:20:17 +0200

    recursive link

--- src/routes/Portfolio.svelte
+++ src/routes/Portfolio.svelte
@@ -24,6 +24,7 @@
           id: "homepage",
           name: "My homepage",
           description: homepageDescription,
+          liveAddress: "https://jupekett.github.io/homepage-svelte/portfolio",
           repository: "https://github.com/jupekett/homepage-svelte",
         }
       ]
